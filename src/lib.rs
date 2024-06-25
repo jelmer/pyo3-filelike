4062f02ed0ae6c018e88dfc155490dcd99ce8a64
Jelmer Vernooĳ <jelmer@jelmer.uk>
2024-06-25 10:31:12 +0100

    Bump pyo3 version

--- src/lib.rs
+++ src/lib.rs
@@ -29,7 +29,7 @@ impl Read for PyBinaryFile {
 impl Write for PyBinaryFile {
     fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
         Python::with_gil(|py| {
-            let bytes = pyo3::types::PyBytes::new(py, buf);
+            let bytes = pyo3::types::PyBytes::new_bound(py, buf);
             self.0.call_method1(py, "write", (bytes, ))?;
             Ok(buf.len())
         })
@@ -75,9 +75,9 @@ impl From<pyo3::PyObject> for PyBinaryFile {
     }
 }
 
-impl From<&PyAny> for PyBinaryFile {
-    fn from(obj: &PyAny) -> Self {
-        PyBinaryFile(obj.into())
+impl From<Bound<'_, PyAny>> for PyBinaryFile {
+    fn from<'a>(obj: Bound<'a, PyAny>) -> Self {
+        PyBinaryFile(obj.unbind())
     }
 }
 
@@ -88,7 +88,7 @@ mod tests {
     #[test]
     fn test_read() {
         Python::with_gil(|py| -> PyResult<()> {
-            let io = py.import("io")?;
+            let io = py.import_bound("io")?;
             let file = io.call_method1("BytesIO", (&b"hello"[..], ))?;
             let mut file = PyBinaryFile::from(file);
             let mut buf = [0u8; 5];
@@ -101,7 +101,7 @@ mod tests {
     #[test]
     fn test_read_notexact() {
         Python::with_gil(|py| -> PyResult<()> {
-            let io = py.import("io")?;
+            let io = py.import_bound("io")?;
             let file = io.call_method1("BytesIO", (&b"hello"[..], ))?;
             let mut file = PyBinaryFile::from(file);
             let mut buf = [0u8; 10];
@@ -115,7 +115,7 @@ mod tests {
     #[test]
     fn test_read_eof() {
         Python::with_gil(|py| -> PyResult<()> {
-            let io = py.import("io")?;
+            let io = py.import_bound("io")?;
             let file = io.call_method1("BytesIO", (&b"hello"[..], ))?;
             let mut file = PyBinaryFile::from(file);
             let mut buf = [0u8; 6];
@@ -128,7 +128,7 @@ mod tests {
     #[test]
     fn test_read_to_end() {
         Python::with_gil(|py| -> PyResult<()> {
-            let io = py.import("io")?;
+            let io = py.import_bound("io")?;
             let file = io.call_method1("BytesIO", (&b"hello"[..], ))?;
             let mut file = PyBinaryFile::from(file);
             let mut buf = Vec::new();
@@ -141,7 +141,7 @@ mod tests {
     #[test]
     fn test_write() {
         Python::with_gil(|py| {
-            let io = py.import("io")?;
+            let io = py.import_bound("io")?;
             let file = io.call_method1("BytesIO", (&b""[..], ))?;
             let mut file = PyBinaryFile::from(file);
             file.write_all(b"hello ")?;
@@ -154,7 +154,7 @@ mod tests {
     #[test]
     fn test_seek() {
         Python::with_gil(|py| {
-            let io = py.import("io")?;
+            let io = py.import_bound("io")?;
             let file = io.call_method1("BytesIO", (&b"hello"[..], ))?;
             let mut file = PyBinaryFile::from(file);
             file.seek(std::io::SeekFrom::Start(1))?;
@@ -168,7 +168,7 @@ mod tests {
     #[test]
     fn test_flush() {
         Python::with_gil(|py| {
-            let io = py.import("io")?;
+            let io = py.import_bound("io")?;
             let file = io.call_method1("BytesIO", (&b""[..], ))?;
             let mut file = PyBinaryFile::from(file);
             file.write_all(b"hello")?;
