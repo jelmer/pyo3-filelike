e0bc2101251d39d6363f5898754214fb3e473e6a
Jelmer Vernooij <jelmer@jelmer.uk>
2025-03-17 13:53:35 +0000

    Add text file support

--- src/lib.rs
+++ src/lib.rs
@@ -150,6 +150,83 @@ impl From<Bound<'_, PyAny>> for PyBinaryFile {
     }
 }
 
+/// Rust wrapper for a Python text file-like object that implements the `io` protocol.
+///
+/// This wrapper is similar to `PyBinaryFile`, but it assumes that the file is text and
+/// returns the byte representation of the text.
+///
+/// The Python file-like object must have a `read` method that returns either a
+/// `bytes` object or a `str` object.
+///
+/// Seek operations are not supported on text files, since the equivalent Python
+/// file-like objects seek by characters, not bytes.
+#[derive(Debug)]
+pub struct PyTextFile {
+    inner: PyObject,
+    buffer: Vec<u8>
+}
+
+impl PyTextFile {
+    /// Create a new `PyTextFile` from a Python file-like object and an encoding.
+    pub fn new(file: PyObject) -> PyResult<Self> {
+        Ok(PyTextFile{ inner: file, buffer: Vec::new() })
+    }
+}
+
+impl Read for PyTextFile {
+    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
+        Python::with_gil(|py| {
+            if self.buffer.len() >= buf.len() {
+                buf.copy_from_slice(&self.buffer[..buf.len()]);
+                self.buffer.drain(..buf.len());
+                return Ok(buf.len());
+            }
+            let text = self.inner.call_method1(py, "read", (buf.len() - self.buffer.len(), ))?;
+            let extra = if let Ok(t) = text.extract::<&str>(py) {
+                t.as_bytes()
+            } else {
+                text.extract::<&[u8]>(py)?
+            };
+
+            self.buffer.extend_from_slice(extra);
+
+            let len = std::cmp::min(self.buffer.len(), buf.len());
+            buf[..len].copy_from_slice(&self.buffer[..len]);
+            self.buffer.drain(..len);
+            Ok(len)
+        })
+    }
+}
+
+impl From<pyo3::PyObject> for PyTextFile {
+    fn from(obj: pyo3::PyObject) -> Self {
+        PyTextFile::new(obj).unwrap()
+    }
+}
+
+impl From<Bound<'_, PyAny>> for PyTextFile {
+    fn from(obj: Bound<'_, PyAny>) -> Self {
+        PyTextFile::new(obj.into()).unwrap()
+    }
+}
+
+impl ToPyObject for PyTextFile {
+    fn to_object(&self, py: Python) -> PyObject {
+        self.inner.clone_ref(py)
+    }
+}
+
+impl Clone for PyTextFile{
+    fn clone(&self) -> Self {
+        Python::with_gil(|py| {
+            PyTextFile::from(self.inner.clone_ref(py))
+        })
+    }
+}
+
+
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -246,5 +323,24 @@ mod tests {
             Ok::<(), PyErr>(())
         }).unwrap();
     }
+
+    #[test]
+    fn test_read_text() {
+        Python::with_gil(|py| -> PyResult<()> {
+            let io = py.import_bound("io")?;
+            let file = io.call_method1("StringIO", ("hello world", ))?;
+            let mut file = PyTextFile::from(file);
+            let mut buf = [0u8; 5];
+            file.read_exact(&mut buf)?;
+            assert_eq!(&buf, b"hello");
+            file.read_exact(&mut buf)?;
+            assert_eq!(&buf, b" worl");
+            let mut buf = Vec::new();
+            file.read_to_end(&mut buf).unwrap();
+            assert_eq!(&buf, b"d");
+            Ok(())
+        }).unwrap();
+    }
+
 
 }
