7f9d93cd1d451dfda1908f270b3aaaa73b267071
Jelmer Vernooij <jelmer@jelmer.uk>
2025-05-22 09:27:40 +0000

    Bump pyo3 to 0.25

--- src/lib.rs
+++ src/lib.rs
@@ -13,9 +13,16 @@ use std::borrow::Cow;
 #[derive(Debug)]
 pub struct PyBinaryFile(pyo3::PyObject);
 
-impl ToPyObject for PyBinaryFile {
-    fn to_object(&self, py: Python) -> PyObject {
-        self.0.clone_ref(py)
+impl<'py> IntoPyObject<'py> for PyBinaryFile {
+    type Target = PyAny;
+
+    type Output = Bound<'py, PyAny>;
+
+    type Error = PyErr;
+
+    fn into_pyobject(self, py: Python<'py>) -> Result<Self::Output, Self::Error> {
+        let obj = self.0.clone_ref(py);
+        Ok(obj.into_bound(py))
     }
 }
 
@@ -42,7 +49,7 @@ impl Read for PyBinaryFile {
 impl Write for PyBinaryFile {
     fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
         Python::with_gil(|py| {
-            let bytes = pyo3::types::PyBytes::new_bound(py, buf);
+            let bytes = pyo3::types::PyBytes::new(py, buf);
             self.0.call_method1(py, "write", (bytes, ))?;
             Ok(buf.len())
         })
@@ -188,9 +195,16 @@ impl From<Bound<'_, PyAny>> for PyTextFile {
     }
 }
 
-impl ToPyObject for PyTextFile {
-    fn to_object(&self, py: Python) -> PyObject {
-        self.inner.clone_ref(py)
+impl<'py> IntoPyObject<'py> for PyTextFile {
+    type Target = PyAny;
+
+    type Output = Bound<'py, PyAny>;
+
+    type Error = PyErr;
+
+    fn into_pyobject(self, py: Python<'py>) -> Result<Self::Output, Self::Error> {
+        let obj = self.inner.clone_ref(py);
+        Ok(obj.into_bound(py))
     }
 }
 
@@ -212,7 +226,7 @@ mod tests {
     #[test]
     fn test_read() {
         Python::with_gil(|py| -> PyResult<()> {
-            let io = py.import_bound("io")?;
+            let io = py.import("io")?;
             let file = io.call_method1("BytesIO", (&b"hello"[..], ))?;
             let mut file = PyBinaryFile::from(file);
             let mut buf = [0u8; 5];
@@ -225,7 +239,7 @@ mod tests {
     #[test]
     fn test_read_notexact() {
         Python::with_gil(|py| -> PyResult<()> {
-            let io = py.import_bound("io")?;
+            let io = py.import("io")?;
             let file = io.call_method1("BytesIO", (&b"hello"[..], ))?;
             let mut file = PyBinaryFile::from(file);
             let mut buf = [0u8; 10];
@@ -239,7 +253,7 @@ mod tests {
     #[test]
     fn test_read_eof() {
         Python::with_gil(|py| -> PyResult<()> {
-            let io = py.import_bound("io")?;
+            let io = py.import("io")?;
             let file = io.call_method1("BytesIO", (&b"hello"[..], ))?;
             let mut file = PyBinaryFile::from(file);
             let mut buf = [0u8; 6];
@@ -252,7 +266,7 @@ mod tests {
     #[test]
     fn test_read_to_end() {
         Python::with_gil(|py| -> PyResult<()> {
-            let io = py.import_bound("io")?;
+            let io = py.import("io")?;
             let file = io.call_method1("BytesIO", (&b"hello"[..], ))?;
             let mut file = PyBinaryFile::from(file);
             let mut buf = Vec::new();
@@ -265,7 +279,7 @@ mod tests {
     #[test]
     fn test_write() {
         Python::with_gil(|py| {
-            let io = py.import_bound("io")?;
+            let io = py.import("io")?;
             let file = io.call_method1("BytesIO", (&b""[..], ))?;
             let mut file = PyBinaryFile::from(file);
             file.write_all(b"hello ")?;
@@ -278,7 +292,7 @@ mod tests {
     #[test]
     fn test_seek() {
         Python::with_gil(|py| {
-            let io = py.import_bound("io")?;
+            let io = py.import("io")?;
             let file = io.call_method1("BytesIO", (&b"hello"[..], ))?;
             let mut file = PyBinaryFile::from(file);
             file.seek(std::io::SeekFrom::Start(1))?;
@@ -292,7 +306,7 @@ mod tests {
     #[test]
     fn test_flush() {
         Python::with_gil(|py| {
-            let io = py.import_bound("io")?;
+            let io = py.import("io")?;
             let file = io.call_method1("BytesIO", (&b""[..], ))?;
             let mut file = PyBinaryFile::from(file);
             file.write_all(b"hello")?;
@@ -305,7 +319,7 @@ mod tests {
     #[test]
     fn test_read_text() {
         Python::with_gil(|py| -> PyResult<()> {
-            let io = py.import_bound("io")?;
+            let io = py.import("io")?;
             let file = io.call_method1("StringIO", ("hello world", ))?;
             let mut file = PyTextFile::from(file);
             let mut buf = [0u8; 5];
